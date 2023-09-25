ceda72d45ca9d05cb07157c86efb802326169de9
MaetelARX <pjelev8@gmail.com>
2023-09-25 17:21:06 +0300

    SoftUni Advanced

---
+++ Streams, Files and Directories - Lab/05. Extract Special Bytes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader binaryReader = new StreamReader(binaryFilePath),
+                bytesToExtract = new StreamReader(bytesFilePath))
+            {
+                byte[] binaryFileBytes = File.ReadAllBytes(binaryFilePath);
+                List<string> bytesList = new List<String>();
+
+                while (!binaryReader.EndOfStream)
+                {
+                    bytesList.Add(binaryReader.ReadLine());
+                }
+
+                foreach (var item in binaryFileBytes)
+                {
+                    if (bytesList.Contains(item.ToString()))
+                    {
+                        sb.AppendLine(item.ToString());
+                    }
+                }
+            }
+
+            using (StreamWriter file = new StreamWriter(outputPath))
+            {
+                file.WriteLine(sb.ToString().Trim());
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories - Lab/06. Split, Merge Binary Files/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace SplitMergeBinaryFile
+{
+    public class SplitMergeBinaryFile
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"..\..\..\Files\example.txt";
+            string joinedFilePath = @"..\..\..\Files\example-joined.txt";
+            string partOnePath = @"..\..\..\Files\part-1.txt";
+            string partTwoPath = @"..\..\..\Files\part-2.txt";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using (StreamReader originalFile = new StreamReader(sourceFilePath))
+            using (StreamWriter firstFile = new StreamWriter(partOneFilePath))
+            using (StreamWriter secondFile = new StreamWriter(partTwoFilePath))
+            {
+                byte[] buffer = originalFile
+                    .ReadToEnd()
+                    .Split(", ")
+                    .Select(byte.Parse)
+                    .ToArray();
+                int numToAdd = 0;
+
+                if (buffer.Length % 2 == 1)
+                {
+                    numToAdd++;
+                }
+
+                for (int i = 0; i < buffer.Length / 2 + numToAdd; i++)
+                {
+                    firstFile.Write(buffer[i] + ", ");
+                }
+
+                for (int i = buffer.Length / 2 + numToAdd; i < buffer.Length; i++)
+                {
+                    secondFile.Write(buffer[i] + ", ");
+                }
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (StreamReader firstFile = new StreamReader(partOneFilePath))
+            using (StreamReader secondFile = new StreamReader(partTwoFilePath))
+            using (StreamWriter mergedFile = new StreamWriter(joinedFilePath))
+            {
+                string firstPart = firstFile
+                    .ReadToEnd();
+
+                string secondPart = secondFile
+                    .ReadToEnd();
+
+
+                mergedFile.WriteLine($"{firstPart}, {secondPart}");
+            }
+        }
+    }
+}
