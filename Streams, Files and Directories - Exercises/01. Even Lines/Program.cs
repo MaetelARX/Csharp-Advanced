8d450ac93f1702eb7b794daecdbf6b8646c8e9f8
MaetelARX <pjelev8@gmail.com>
2023-09-26 03:10:29 +0300

    SoftUni Advanced

---
+++ Streams, Files and Directories - Exercises/01. Even Lines/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Text;
+
+    public class EvenLines
+    {
+        public static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            using StreamReader textToRead = new StreamReader(inputFilePath);
+
+            int counter = 0;
+            string line = string.Empty;
+            while ((line = textToRead.ReadLine()) != null)
+            {
+                if (counter % 2 == 0)
+                {
+                    string replacedSymbols = ReplaceSymbols(line);
+                    string reversedLine = ReverseSymbols(replacedSymbols);
+                    sb.AppendLine(reversedLine);
+                }
+
+                counter++;
+            }
+
+            return sb.ToString();
+        }
+
+
+        public static string ReplaceSymbols(string line)
+        {
+            StringBuilder sb = new StringBuilder(line);
+            char[] symbolsToChange = { '-', ',', '.', '!', '?' };
+
+            foreach (char symbol in symbolsToChange)
+            {
+                sb.Replace(symbol, '@');
+            }
+
+            return sb.ToString();
+        }
+
+        private static string ReverseSymbols(string line)
+        {
+            string[] reverseLine = line.Split(" ").Reverse().ToArray();
+
+            return string.Join(" ", reverseLine);
+        }
+    }
+}
